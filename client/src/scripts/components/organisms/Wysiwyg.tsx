c43825243685c328a5c130288ae8cc7b40b9050d
Maikel van Veen <maikeloo10@hotmail.com>
2019-05-06 14:40:42 +0200

    feat(wysiwig): add wysiwig component

---
+++ client/src/scripts/components/organisms/Wysiwyg.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface Props {}
+
+export class Wysiwig extends React.Component<Props> {
+    public render() {
+        return (
+            <div className={`Wysiwig`}>
+                Wysiwig
+            </div>
+        )
+    }
+}
--- client/src/scripts/index.tsx
+++ client/src/scripts/index.tsx
@@ -1,9 +1,13 @@
 import 'babel-polyfill'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Wysiwig } from './components/organisms/Wysiwyg'
 
 const App: React.SFC = () => (
-    <div>This is React</div>
+    <div>
+        <Wysiwig />
+        {/* Here comes the tags selector organism */}
+    </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('react-root'))
