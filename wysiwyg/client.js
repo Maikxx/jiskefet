0dbf0618c430ecae011c242d23b77fcabe4e2378
Robin <robinstut@213.226.028.145.hva.nl>
2019-05-07 11:43:58 +0200

    adding wysiwyg functions

--- wysiwyg/client.js
+++ wysiwyg/client.js
@@ -10,6 +10,8 @@ form.addEventListener('submit', function (e) {
     newValue = convItalic(newValue)
     newValue = convStrikethrough(newValue)
     newValue = convHeading(newValue)
+    newValue = convUl(newValue)
+    newValue = convOl(newValue)
     output.innerHTML = (newValue);
 })
 
@@ -53,4 +55,68 @@ function convHeading(input) {
         return ``;
     });
     return rem;
+}
+
+function convUl(input) {
+    rx1 = /(<ul>)(.*)(<\/ul>)/g
+    rx2 = /(<[\/]*ul>)/g
+    rx3 = /(<li>)/g
+    rx4 = /(<\/li>)/g
+
+    let parent = input.replace(rx1, (...x) => {
+
+        let li1 = x[0].replace(rx2, (...r) => {
+            return ``;
+        });
+
+        let li2 = li1.replace(rx3, (...r2) => {
+            return `⋅⋅*`;
+        });
+
+        let li3 = li2.replace(rx4, (...x) => {
+            return `<br>`;
+        });
+        return li3
+    });
+
+    return parent;
+}
+
+function convOl(input) {
+    rx1 = /(<ol>)(.*)(<\/ol>)/g
+    rx2 = /(<[\/]*ol>)/g
+    rx3 = /(<li>)/g
+    rx4 = /(<\/li>)/g
+    var liCount = 0;
+
+    let parent = input.replace(rx1, (...x) => {
+
+        let li1 = x[0].replace(rx2, (...r) => {
+            return ``;
+        });
+
+        let li2 = li1.replace(rx3, (...r2) => {
+            liCount += 1;
+            return `${liCount}.`;
+        });
+
+        let li3 = li2.replace(rx4, (...x) => {
+            return `<br>`;
+        });
+        return li3
+    });
+
+    return parent;
+}
+
+
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
 }
