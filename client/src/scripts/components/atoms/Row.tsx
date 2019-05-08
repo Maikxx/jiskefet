8a901659e0bed7b52ca4905625381b96a3caf50e
Maikel van Veen <maikeloo10@hotmail.com>
2019-05-08 11:12:56 +0200

    feat(row): add space between

--- client/src/scripts/components/atoms/Row.tsx
+++ client/src/scripts/components/atoms/Row.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react'
 
-interface Props {}
+interface Props {
+    spaceBetween?: boolean
+}
 
 export class Row extends React.Component<Props> {
     public render() {
         const { children } = this.props
 
         return (
-            <div className={`Row`}>
+            <div className={this.getClassName()}>
                 {children}
             </div>
         )
+    }
+
+    private getClassName() {
+        const { spaceBetween } = this.props
+
+        return `Row${spaceBetween ? ' Row--space-between' : ''}`
     }
 }
