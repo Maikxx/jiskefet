e75de709fd0148c758f660f0054217d611d39f34
RobinStut <robin.stut@hav.nl>
2019-05-09 15:48:51 +0200

    fixing italic markdown

--- wysiwyg/client.js
+++ wysiwyg/client.js
@@ -2,12 +2,6 @@ console.log('working');
 
 const form = document.getElementById('form')
 const output = document.getElementById('markdownOutput')
-// var createLink = document.getElementById('createLink');
-// createLink.onclick = linkChange;
-// var createImage = document.getElementById('createImage');
-// createImage.onclick = imageChange;
-
-
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -146,4 +140,110 @@ function convBq(input) {
         return ``;
     });
     return rem;
+}
+
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    if (e.path[0].className === 'editor') {
+        var currentInputValue = String(e.path[0].innerText + e.key)
+        var secondLastChar = currentInputValue.slice(-2, -1)
+        var thirdLastChar = currentInputValue.slice(-3, -2)
+        var lastSixChars = currentInputValue.slice(-6)
+        if (String(e.key)) {
+            name(e.key, secondLastChar, lastSixChars, e, thirdLastChar)
+        }
+    }
+}
+
+function name(newChar, lastChar, lastSixChars, e, secondLast) {
+    var specialChars = ['*', '_', '~', '+', '-', '[', '(', '#']
+
+
+
+    setTimeout(function () {
+        var textField = document.getElementsByClassName('editor')[0]
+        var checkTag = textField.innerHTML.slice(-8)
+        var rx1 = /(<\/[\w\d]*>)/g
+        let get = checkTag.replace(rx1, (...x) => {
+            checkTag = x[0]
+        });
+
+
+
+        if (specialChars.includes(newChar)) {
+            console.log(' ');
+            var closingTagFix = (String(textField.innerHTML).slice(-(checkTag.length + 3)));
+            closingTagFix = closingTagFix.slice(0, 1)
+            var duplicate = (newChar === lastChar);
+            var caretNum = textField.innerText.length
+
+
+            if ((newChar === '*' || newChar === '_') && (duplicate === false) && (checkTag.includes('</') === false)) {
+                console.log('italic');
+                console.log('em toegevoegd');
+                textField.innerHTML = textField.innerHTML.slice(0, -1) + `<em>${newChar}</em>`
+                placeCaretAtEnd(textField);
+            }
+            if ((newChar === '*' || newChar === '_') && (duplicate === true) && (checkTag.includes('</') === false)) {
+                console.log('bold');
+                textField.innerHTML = textField.innerHTML.slice(0, -1) + `<b>${newChar}</b>`
+                placeCaretAtEnd(textField);
+            }
+            if ((newChar === '~') && (duplicate === true)) {
+                console.log('strikethrough');
+            }
+            if ((newChar === '+' || newChar === '-') && (lastChar === '/')) {
+                console.log('bullitpoint');
+            }
+            if ((newChar === '[') && (lastChar !== '!')) {
+                console.log('link');
+            }
+            if ((newChar === '[') && (lastChar === '!')) {
+                console.log('image');
+            }
+            if (newChar === '#') {
+                var countHeadingLvl = String(lastSixChars.replace(/[^#]/g, "").length);
+                console.log('h' + countHeadingLvl);
+            }
+        }
+
+        if ((checkTag.includes('</') && (specialChars.includes(lastChar) && (closingTagFix !== '>') && (e.keyCode !== 32)))) {
+
+            var FirstInnerHtmlValue = String(textField.innerHTML).slice(0, -(checkTag.length + 1))
+            console.log(`first = ${FirstInnerHtmlValue}`);
+            var LastInnerHtmlValue = String(textField.innerHTML).slice(-(checkTag.length))
+            console.log(`last = ${LastInnerHtmlValue}`);
+            textField.innerHTML = FirstInnerHtmlValue + LastInnerHtmlValue + newChar;
+            placeCaretAtEnd(textField);
+
+        }
+
+        if (newChar === 'Backspace') {
+            console.log('backspace');
+        }
+    }, 0)
+}
+
+
+
+
+function placeCaretAtEnd(el) {
+    console.log(el);
+    el.focus();
+    if (typeof window.getSelection != "undefined" &&
+        typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
 }
