7e9d542edebe777093cc584432179905bc3a6c38
Maikel van Veen <maikeloo10@hotmail.com>
2019-05-09 10:09:39 +0200

    refactor(Language): move language files from app to LanguageProvider

--- client/src/scripts/App.tsx
+++ client/src/scripts/App.tsx
@@ -7,40 +7,8 @@ import { CreateTag } from './components/molecules/CreateTag'
 import { Attachment } from './components/organisms/Attachment'
 import { Button } from './components/atoms/Button'
 import { Language } from './types/Language'
-import { LanguageContext, LANGUAGE_OPTIONS } from './components/LanguageProvider'
+import { LanguageContext, LANGUAGE_OPTIONS, LANGUAGE_MAP } from './components/LanguageProvider'
 import { Selector } from './components/atoms/Selector'
-
-import i18n_de from '../i18n/de.i18n.json'
-import i18n_en from '../i18n/en.i18n.json'
-import i18n_es from '../i18n/es.i18n.json'
-import i18n_hi from '../i18n/hi.i18n.json'
-import i18n_ru from '../i18n/ru.i18n.json'
-import i18n_zh from '../i18n/zh.i18n.json'
-import i18n_pl from '../i18n/pl.i18n.json'
-import i18n_pt from '../i18n/pt.i18n.json'
-import i18n_ur from '../i18n/ur.i18n.json'
-import i18n_uk from '../i18n/uk.i18n.json'
-import i18n_it from '../i18n/it.i18n.json'
-import i18n_fr from '../i18n/fr.i18n.json'
-import i18n_ar from '../i18n/ar.i18n.json'
-import i18n_tr from '../i18n/tr.i18n.json'
-
-const languageMap = {
-    de: i18n_de,
-    en: i18n_en,
-    es: i18n_es,
-    hi: i18n_hi,
-    ru: i18n_ru,
-    zh: i18n_zh,
-    pl: i18n_pl,
-    pt: i18n_pt,
-    ur: i18n_ur,
-    uk: i18n_uk,
-    it: i18n_it,
-    fr: i18n_fr,
-    ar: i18n_ar,
-    tr: i18n_tr,
-}
 
 interface State {
     addedTags: Tag[]
@@ -50,7 +18,7 @@ interface State {
 export class App extends React.Component<{}, State> {
     public state: State = {
         addedTags: [],
-        language: languageMap.en,
+        language: LANGUAGE_MAP.en,
     }
 
     public render() {
@@ -110,7 +78,7 @@ export class App extends React.Component<{}, State> {
 
     private onChangeLanguage = (value?: string) => {
         if (value) {
-            this.setState({ language: languageMap[value] })
+            this.setState({ language: LANGUAGE_MAP[value] })
         }
     }
 
--- client/src/scripts/components/LanguageProvider.tsx
+++ client/src/scripts/components/LanguageProvider.tsx
@@ -1,7 +1,21 @@
-import i18n_en from '../../i18n/en.i18n.json'
 import React from 'react'
+import i18n_en from '../../i18n/en.i18n.json'
+import i18n_de from '../../i18n/de.i18n.json'
+import i18n_es from '../../i18n/es.i18n.json'
+import i18n_hi from '../../i18n/hi.i18n.json'
+import i18n_ru from '../../i18n/ru.i18n.json'
+import i18n_zh from '../../i18n/zh.i18n.json'
+import i18n_pl from '../../i18n/pl.i18n.json'
+import i18n_pt from '../../i18n/pt.i18n.json'
+import i18n_ur from '../../i18n/ur.i18n.json'
+import i18n_uk from '../../i18n/uk.i18n.json'
+import i18n_it from '../../i18n/it.i18n.json'
+import i18n_fr from '../../i18n/fr.i18n.json'
+import i18n_ar from '../../i18n/ar.i18n.json'
+import i18n_tr from '../../i18n/tr.i18n.json'
 
 export const LanguageContext = React.createContext(i18n_en)
+
 export const LANGUAGE_OPTIONS = [
     { name: 'Arabic', code: 'AR', disabled: false },
     { name: 'Chinese Mandarin', code: 'ZH', disabled: false },
@@ -18,3 +32,20 @@ export const LANGUAGE_OPTIONS = [
     { name: 'Ukrainian', code: 'UK', disabled: false },
     { name: 'Urdu', code: 'UR', disabled: false },
 ]
+
+export const LANGUAGE_MAP = {
+    de: i18n_de,
+    en: i18n_en,
+    es: i18n_es,
+    hi: i18n_hi,
+    ru: i18n_ru,
+    zh: i18n_zh,
+    pl: i18n_pl,
+    pt: i18n_pt,
+    ur: i18n_ur,
+    uk: i18n_uk,
+    it: i18n_it,
+    fr: i18n_fr,
+    ar: i18n_ar,
+    tr: i18n_tr,
+}
